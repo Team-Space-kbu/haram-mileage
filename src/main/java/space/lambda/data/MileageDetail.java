44758c6313574554111351f26eba63a5bfd5253d
mookseong <kmkook12312@gmail.com>
2024-06-24 17:30:33 +0900

    feat : 정규식을 통한 데이터 기록 확인

--- src/main/java/space/lambda/data/MileageDetail.java
+++ src/main/java/space/lambda/data/MileageDetail.java
@@ -5,24 +5,29 @@ import static space.lambda.util.TextUtil.getTextContent;
 import lombok.Builder;
 import lombok.Getter;
 import org.w3c.dom.NodeList;
+import space.lambda.model.MileageType;
 
 @Builder
 @Getter
 public class MileageDetail {
+
   private String changeDate;   //변경일자
   private String saleDate;     //판매일자
   private String status;       //구분
   private String point;        //포인트
-  private String note;         //비고
+  private String etc;         //비고
+  private MileageType type;
 
-
-  public static MileageDetail toDetail(NodeList nList2){
+  public static MileageDetail toDetail(NodeList nList2) {
+    String note = getTextContent(nList2, 4);
+    String shop = note.replaceAll("성서대\\.|:|\\(승인\\)|포인트|사용|취소", "").trim();
     return MileageDetail.builder()
         .changeDate(getTextContent(nList2, 0))
         .saleDate(getTextContent(nList2, 1))
         .status(getTextContent(nList2, 2))
         .point(getTextContent(nList2, 3))
-        .note(getTextContent(nList2, 4))
+        .etc(shop)
+        .type(MileageType.findMileageType(shop, note))
         .build();
   }
 }
---
+++ src/main/java/space/lambda/model/MileageType.java
@@ -0,0 +1,49 @@
+package space.lambda.model;
+
+import java.util.regex.Pattern;
+
+public enum MileageType {
+  CAFE,
+  GYM,
+  MART,
+  BOOKSTORE,
+  COPYROOM,
+  STUDENT,
+  ETC;
+
+  private static final String regex = "(\\d{4}).*?-([^-]*)$";
+  private static final Pattern pattern = Pattern.compile(regex);
+
+  public static MileageType findMileageType(String etc, String status) {
+    if (status.contains("조정")) {
+      return MileageType.STUDENT;
+    }
+    switch (etc) {
+      case "북카페" -> {
+        return MileageType.BOOKSTORE;
+      }
+      case "헬스장" -> {
+        return MileageType.GYM;
+      }
+      case "매점", "마트" -> {
+        return MileageType.MART;
+      }
+      case "복사실" -> {
+        return MileageType.COPYROOM;
+      }
+      case "카페", "카페코스테스", "카페테리아" -> {
+        return MileageType.CAFE;
+      }
+      case "교학처" -> {
+        return MileageType.STUDENT;
+      }
+      default -> {
+        if (pattern.matcher(etc).find()) {
+          return MileageType.STUDENT;
+        }
+        return MileageType.ETC;
+      }
+    }
+
+  }
+}
